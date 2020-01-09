6c4fb01a42dcfa4a465f730f0d6c2ac3a3cc9634
ulianazaiczeva <57898235+ulianazaiczeva@users.noreply.github.com>
2020-01-09 21:12:45 +0500

    Create ui_file.py

---
+++ ui_file.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(720, 591)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(300, 280, 89, 25))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PUSH!"))
